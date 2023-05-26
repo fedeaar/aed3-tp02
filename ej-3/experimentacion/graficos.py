c77ab5684a4d2bb2b69b477dcfd09490ec3aa516
fedeaar <fa.arienti@gmail.com>
2023-05-26 01:34:46 -0300

    upd informe

--- ej-3/experimentacion/graficos.py
+++ ej-3/experimentacion/graficos.py
@@ -40,6 +40,31 @@ ax.grid()
 ax.legend(fontsize="xx-large")
 f.savefig("./out/comparacion_asintótica.png", bbox_inches='tight')
 
+df3 = df
+df3["time"] = np.abs(df["time"] - df2["time"])
+
+data = np.polyfit(df3["n"], df3["time"], 2, full=True)
+fit = np.poly1d(data[0]) 
+print(data)
+
+
+f, ax = plt.subplots(figsize=(7, 7))
+ax.scatter(df3["n"], df3["time"])
+plt.plot(df3["n"], fit(df3["n"]), color="green", label="diferencia")
+#sns.regplot(x="n", y="time", data=df3, ax=ax, order=2) #, ci=None)
+ax.set_xscale("linear")
+ax.set_yscale("linear")
+ax.set_xlabel("tamaño de la entrada", **config)
+ax.set_ylabel("tiempo (segundos)", **config)
+ax.tick_params(labelsize=16, size=10)
+ax.grid()
+ax.legend(fontsize="xx-large")
+f.savefig("./out/comparacion_asintótica_diff.png", bbox_inches='tight')
+
+
+
+
+
 df = pd.read_csv("./out/runtime_DSU_optimizado.csv")
 df2 = pd.read_csv("./out/runtime_DSU_suboptimo.csv")
 
